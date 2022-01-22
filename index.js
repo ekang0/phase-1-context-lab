07daa1afff53d4a22595497c75052b2689241c1f
Esther Kang <eskang87@gmail.com>
2022-01-22 15:17:50 -0800

    commit tree 0fc40a76fe292bc34b89dda8dc47e7895ad15f03

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    let employeeRecordObj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employeeRecordObj;
+};
+
+/*
+function createEmployeeRecords(arrayOfArrays){
+    let arrayOfRecords = arrayOfArrays.map(array => createEmployeeRecord(array));
+    //arrayOfObjects
+    return arrayOfRecords;
+}; 
+//Refactor to the below*/
+
+let createEmployeeRecords = function(multiEmployeeData){
+    return multiEmployeeData.map(singleData => createEmployeeRecord(singleData))
+};
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+let createTimeOutEvent = function(dateStamp){
+    let[date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+let hoursWorkedOnDate = function(date){
+    let timeIn = this.timeInEvents[this.timeInEvents.findIndex(timeInElement => timeInElement.date === date)].hour;
+    let timeOut = this.timeOutEvents[this.timeOutEvents.findIndex(timeOutElement => timeOutElement.date === date)].hour;
+    return (timeOut - timeIn)/100;
+};
+
+let wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
+/*
+let allWagesFor = function (){
+    let allDates = this.timeInEvents.map(objElement => objElement.date);
+    let payOwedByDate = allDates.map(date => wagesEarnedOnDate.call(this, date));
+    let totalPayAllDatesNum = payOwedByDate.reduce((previousVal, currentVal) => previousVal + currentVal);
+    return totalPayAllDatesNum;
+}
+//can probably refactor more to combine payOwedByDate and totalPayAllDatesNum 
+*/
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +86,15 @@ const allWagesFor = function () {
     return payable
 }
 
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+       return srcArray.find(elementArray => {
+        return elementArray.firstName === firstName
+    })
+};
+
+let calculatePayroll = function (arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce((previousVal, currentVal) => {
+        return previousVal + allWagesFor.call(currentVal)
+    }, 0) ;
+};
